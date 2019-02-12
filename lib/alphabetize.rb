6dd54ebd88551aac1b593e56237ace3fdf271b70
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-02-12 18:17:30 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,29 @@
 def alphabetize(arr)
-  # code here
+  finalArr = [];
+  
+  arr.each {|word|
+    firstLetter = word[0]
+    firstLetterIdx = idxFinder(firstLetter)
+    #rethink this part
+      if finalArr.length == 0
+        finalArr.push(word)
+      else
+      finalArr.each{|finalWord|
+        
+      }
+      end
+  }
+end
+
+def idxFinder(firstLetter)
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alphaIdx = nil
+  
+  alphabet.each_with_index{|letter, i|
+    if letter == firstLetter
+      alphaIdx = i
+    end
+  }
+  
+  return alphaIdx
 end
