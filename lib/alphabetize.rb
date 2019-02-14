69af4ce078ff052826c274ae6d99916ec24dc268
peidansauyukjuk <smei8800@gmail.com>
2019-02-14 00:10:17 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,19 @@
+require "pry"
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  sorted = arr.sort_by do |phrase|
+    phrase_arr = phrase.split("")
+    new_phrase = []
+    phrase_arr.each do |letter|
+      if ESPERANTO_ALPHABET.include?(letter)
+        new_phrase << ESPERANTO_ALPHABET.index(letter) + 1
+      else
+        new_phrase << 0
+      end
+    end
+    new_phrase
+  end
+  sorted
 end
