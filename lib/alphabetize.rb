7a7bab1a4910d90c8986180f655dd59d25501d96
mnkillebr <killebrew.biz@gmail.com>
2019-02-18 22:07:05 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
-def alphabetize(arr)
-  # code here
+require "pry"
+ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(array)
+  array.sort_by do |key|
+    key.split("").collect do |value|
+    ESPERANTO_ALPHABET.index(value)
+    
+    end
+  end
 end
